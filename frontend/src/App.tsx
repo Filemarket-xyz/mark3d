29c2c73e77ce8033f77cfc5df52dc482b8229f30
santalov <santalov.stas@gmail.com>
2022-10-20 17:44:24 +0300

    rollback App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
 import React, { FC } from 'react'
 import { Providers } from './providers'
-import { Container } from './app/UIkit'
+import { ConnectButton } from './app/components'
 
 export const App: FC = () => {
   return (
     <Providers>
       <div className="App">
-        <Container>
-          <div
-            style={{
-              width: '100%',
-              height: '400px',
-              backgroundColor: 'lightblue'
-            }}
-          >
-          </div>
-        </Container>
+        <ConnectButton/>
       </div>
     </Providers>
   )
