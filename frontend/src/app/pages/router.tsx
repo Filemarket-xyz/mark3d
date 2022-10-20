b88ba8c715db0f7205dbc2973efafb44b6f7dbc7
George <ancient.gekob@gmail.com>
2022-10-20 18:57:47 +0300

    add outlet to routing domain

--- frontend/src/app/pages/router.tsx
+++ frontend/src/app/pages/router.tsx
@@ -1,9 +1,23 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouteObject, Outlet as RouterOutlet } from 'react-router-dom'
+import { Outlet } from '../components/Outlet/Outlet'
 import ExplorerPage from './ExplorerPage/ExplorerPage'
+
+const routes: RouteObject[] = [
+  {
+    path: 'explorer',
+    element: <ExplorerPage/>
+  }
+]
 
 export const router = createBrowserRouter([
   {
-    path: '/explorer',
-    element: <ExplorerPage/>
+    path: '/',
+    element: (
+    <>
+      <Outlet>
+        <RouterOutlet/>
+      </Outlet>
+    </>),
+    children: routes
   }
 ])
