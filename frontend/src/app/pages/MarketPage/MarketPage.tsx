5c6ca76d3d1cccfce76a0be9c52a2acda3a34a21
George Kobunov <ancient.gekob@gmail.com>
2022-11-02 14:32:38 +0300

    implemented nft card, removed txt inside button to make button more reussable

--- frontend/src/app/pages/MarketPage/MarketPage.tsx
+++ frontend/src/app/pages/MarketPage/MarketPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { styled } from '../../../styles'
+import { Button, textVariant, Txt } from '../../UIkit'
 import { PageLayout } from '../../UIkit/PageLayout'
 import cardImg from './img/cardImg.jpg'
-
-// const myAnim = keyframes({
-//   '0%': { bottom: '-48px' },
-//   '100%': { bottom: '0' }
-// })
+import userimg from './img/userImg.jpg'
 
 const CardControls = styled('div', {
   width: '100%',
@@ -20,7 +17,9 @@ const CardControls = styled('div', {
   right: 0,
   transform: 'translateY(0)',
   transition: 'all 0.25s ease-in-out',
-  bottom: '-48px'
+  bottom: '-48px',
+  padding: '$3 11px',
+  transitionDelay: '0.35s'
 })
 
 const styles: any = {
@@ -31,10 +30,11 @@ const styles: any = {
   overflow: 'hidden'
 }
 styles[`&:hover ${CardControls.selector}`] = {
-  transform: 'translateY(-48px)'
+  transform: 'translateY(-48px)',
+  transitionDelay: '0s'
 }
 
-const MarketCard = styled('div', styles)
+const Card = styled('div', styles)
 
 const CardImg = styled('img', {
   width: '100%',
@@ -45,13 +45,78 @@ const CardImg = styled('img', {
   border: '1px solid rgba(255, 255, 255, 0.5)'
 })
 
+const CardTitle = styled('h5', {
+  ...textVariant('primary2').true,
+  marginBottom: '$1'
+})
+
+const CardCollection = styled('p', {
+  ...textVariant('secondary3').true,
+  marginBottom: '$1'
+})
+
+const PriceInfo = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: '$3'
+})
+
+const UserContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$1',
+  color: '$gray500'
+})
+
+const UserImg = styled('img', {
+  width: '20px',
+  height: '20px',
+  borderRadius: '50%'
+})
+
+const UserName = styled('p', {
+  ...textVariant('primary3').true,
+  lineHeight: '50%'
+})
+
+const Price = styled('span', {
+  ...textVariant('primary1'),
+  color: '$blue900',
+  fontWeight: '600'
+})
+
+const ButtonContainer = styled('div', {
+  display: 'flex',
+  justifyContent: 'center'
+})
+
 export default function MarketPage() {
   return (
     <PageLayout>
-      <MarketCard>
+      <Card>
         <CardImg src={cardImg} />
-        <CardControls></CardControls>
-      </MarketCard>
+        <CardControls>
+          <CardTitle>Ultra mega super VR Glasses...</CardTitle>
+          <CardCollection>VR Glasses collection</CardCollection>
+          <PriceInfo>
+            <UserContainer>
+              <UserImg src={userimg} />
+              <UserName>UnderKong</UserName>
+            </UserContainer>
+            <Price>0.666 ETH</Price>
+          </PriceInfo>
+          <ButtonContainer>
+            <Button
+              primary
+              small={'true'}
+              css={{ marginLeft: 'auto', marginRight: 'auto' }}
+            >
+              <Txt primary3>Buy now</Txt>
+            </Button>
+          </ButtonContainer>
+        </CardControls>
+      </Card>
     </PageLayout>
   )
 }
