d810dbbea279ae89ce33e909b26408f77ffef238
George Kobunov <ancient.gekob@gmail.com>
2022-11-02 15:09:40 +0300

    refactored card, now it can get props

--- frontend/src/app/pages/MarketPage/MarketPage.tsx
+++ frontend/src/app/pages/MarketPage/MarketPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import NFTCard from '../../components/NFTCard/NFTCard'
 import { PageLayout } from '../../UIkit/PageLayout'
+import cardImg from './img/cardImg.jpg'
+import userimg from './img/userImg.jpg'
 
 export default function MarketPage() {
   return (
     <PageLayout>
-      <NFTCard></NFTCard>
+      <NFTCard
+        collection='VR Glasses collection'
+        imageURL={cardImg}
+        price={0.66666}
+        title='Ultra mega super VR Glasses... '
+        user={{ img: userimg, username: 'UnderKong' }}
+      ></NFTCard>
     </PageLayout>
   )
 }
