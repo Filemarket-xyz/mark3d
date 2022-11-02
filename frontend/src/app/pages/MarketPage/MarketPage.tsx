5eb6d5b83a3131b7b1345c9dc0415a218765c76b
George Kobunov <ancient.gekob@gmail.com>
2022-11-02 19:11:01 +0300

    implemented collection card

--- frontend/src/app/pages/MarketPage/MarketPage.tsx
+++ frontend/src/app/pages/MarketPage/MarketPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { styled } from '../../../styles'
-import NFTCard from '../../components/NFTCard/NFTCard'
+import CollectionCard from '../../components/MarketCard/CollectionCard'
+import NFTCard from '../../components/MarketCard/NFTCard'
 import { PageLayout } from '../../UIkit/PageLayout'
 import cardImg from './img/cardImg.jpg'
 import userimg from './img/userImg.jpg'
 import Tabs from './Tabs'
+import collectionIcon from './img/icon.jpg'
 
 const TabsContainer = styled('div', {
   marginBottom: '$4'
@@ -23,6 +25,11 @@ export default function MarketPage() {
         title='Ultra mega super VR Glasses... '
         user={{ img: userimg, username: 'UnderKong' }}
       ></NFTCard>
+      <CollectionCard
+        description={<>Mark3d NFT collection for<br/> 3D Internet and virtual worlds</>}
+        iconURL={collectionIcon}
+        imageUrl={cardImg}
+      />
     </PageLayout>
   )
 }
