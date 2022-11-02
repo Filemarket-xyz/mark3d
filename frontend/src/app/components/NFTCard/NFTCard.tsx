47d6984006fa87f272231a94ad8d6ea113bbe32f
George Kobunov <ancient.gekob@gmail.com>
2022-11-02 18:38:39 +0300

    now we have basic card, other cards like nft, collections, creators, etc will apply existing styles

--- frontend/src/app/components/NFTCard/NFTCard.tsx
+++ frontend/src/app/components/NFTCard/NFTCard.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { styled } from '../../../styles'
 import { Button, textVariant, Txt } from '../../UIkit'
+import BasicCard from './BasicCard'
 
 const CardControls = styled('div', {
   width: '100%',
@@ -41,22 +42,8 @@ const generateHoverStylesForCard = () => {
     transitionDelay: '0s'
   }
 
-  hoverStyles['&:hover'] = {
-    filter: 'drop-shadow(0px 12px 25px rgba(19, 19, 45, 0.35))'
-  }
-
   return hoverStyles
 }
-
-const Card = styled('div', {
-  maxWidth: '255px',
-  height: '320px',
-  borderRadius: '$3',
-  position: 'relative',
-  overflow: 'hidden',
-  filter: 'drop-shadow(0px 4px 15px rgba(19, 19, 45, 0.1))',
-  ...generateHoverStylesForCard()
-})
 
 const CardImg = styled('img', {
   width: '100%',
@@ -118,6 +105,10 @@ export interface Props {
   }
   price: number
 }
+
+const Card = styled(BasicCard, {
+  ...generateHoverStylesForCard()
+})
 
 export default function NFTCard(props: Props) {
   const formatPrice = useCallback((price: number) => {
