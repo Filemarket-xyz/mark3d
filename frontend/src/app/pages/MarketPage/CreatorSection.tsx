1b50adbe9da4882145975d7ccec2c9f9a9ebbffa
George Kobunov <ancient.gekob@gmail.com>
2022-11-03 18:39:03 +0300

    now there is outlet inside market page

---
+++ frontend/src/app/pages/MarketPage/CreatorSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreatorSection() {
+  return (
+    <div>CreatorSection</div>
+  )
+}
--- frontend/src/app/pages/MarketPage/MarketPage.tsx
+++ frontend/src/app/pages/MarketPage/MarketPage.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
 import { styled } from '../../../styles'
-import CollectionCard from '../../components/MarketCard/CollectionCard'
-import NFTCard from '../../components/MarketCard/NFTCard'
 import { PageLayout } from '../../UIkit/PageLayout'
-import cardImg from './img/cardImg.jpg'
-import userimg from './img/userImg.jpg'
 import Tabs from './Tabs'
-import collectionIcon from './img/icon.jpg'
-import CreatorCard from '../../components/MarketCard/CreatorCard'
-import bg from './img/bg.jpg'
-import largerUserImg from './img/largerUserImg.jpg'
-import NamespaceCard from '../../components/MarketCard/NamespaceCard'
-import namespace from './img/namespace.jpg'
+import { Outlet, RouteObject } from 'react-router'
+import NftSection from './NftSection'
+import CollectionSection from './CollectionSection'
+import CreatorSection from './CreatorSection'
+import NamespaceSection from './NamespaceSection'
 
 const TabsContainer = styled('div', {
   marginBottom: '$4'
 })
+
+export const marketRoutes: RouteObject[] = [
+  {
+    path: 'nfts',
+    element: <NftSection />
+  },
+  {
+    path: 'collections',
+    element: <CollectionSection />
+  },
+  {
+    path: 'creators',
+    element: <CreatorSection />
+  },
+  {
+    path: 'namespaces',
+    element: <NamespaceSection />
+  }
+]
 
 export default function MarketPage() {
   return (
@@ -23,44 +36,7 @@ export default function MarketPage() {
       <TabsContainer>
         <Tabs />
       </TabsContainer>
-      <NFTCard
-        collection='VR Glasses collection'
-        imageURL={cardImg}
-        price={0.66666}
-        title='Ultra mega super VR Glasses... '
-        user={{ img: userimg, username: 'UnderKong' }}
-      ></NFTCard>
-      <CollectionCard
-        description={
-          <>
-            Mark3d NFT collection for
-            <br /> 3D Internet and virtual worlds
-          </>
-        }
-        iconURL={collectionIcon}
-        imageUrl={cardImg}
-      />
-      <CreatorCard
-        bgImageUrl={bg}
-        description={
-          <>
-            UnderKong.eth Meta Jedi / Council of MetaLegends / Founder of
-            DAOart. UnderKong.eth Meta Jedi / Council of MetaLegends / Founder
-            of DAOart.
-          </>
-        }
-        user={{
-          imageUrl: largerUserImg,
-          name: 'UnderKong',
-          social: '@UnderKong`s twitter'
-        }}
-      />
-      <NamespaceCard
-        imageURL={namespace}
-        price={0.6665}
-        title={'Mark3dNamespace.3d'}
-        user={{ img: userimg, username: 'UnderKong' }}
-      />
+      <Outlet />
     </PageLayout>
   )
 }
