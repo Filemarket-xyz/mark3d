10d60d5d78821ad97b1e5a38d6dd6d1f9f9578d0
rey256 <rey256@medvedosandr@gmail.com>
2022-11-08 01:06:00 +0300

    new file:   backend/src/helpers.rs 	modified:   backend/src/main.rs
    * parse tx logs added

---
+++ backend/src/helpers.rs
@@ -0,0 +1,97 @@
+use web3::{
+    ethabi::{Contract, Log, RawLog},
+    transports::WebSocket,
+    types::{Block, BlockId, BlockNumber, Transaction, TransactionReceipt, H256},
+    Web3,
+};
+
+pub struct OraculConf {
+    pub eth_api_url: String,
+    pub contract_address: String,
+    pub contract: web3::ethabi::Contract,
+}
+
+#[derive(Debug)]
+pub struct TransferFraudReported {
+    pub token_id: u64,
+    pub decided: bool,
+    pub approved: bool,
+}
+
+async fn get_tx_receipt(hash: H256, web3: &Web3<WebSocket>) -> Result<TransactionReceipt, ()> {
+    if let Some(tx) = match web3.eth().transaction_receipt(hash).await {
+        Ok(tx) => tx,
+        Err(_) => return Err(()),
+    } {
+        return Ok(tx);
+    } else {
+        Err(())
+    }
+}
+
+pub async fn get_event_logs(
+    event_name: &str,
+    hash: H256,
+    web3: &Web3<WebSocket>,
+    cont: &Contract,
+) -> Result<Log, ()> {
+    let tx = get_tx_receipt(hash, web3).await?;
+
+    if tx.logs.is_empty() {
+        return Err(());
+    }
+
+    let event = match cont.event(event_name) {
+        Ok(e) => e,
+        Err(_) => return Err(()),
+    };
+
+    match event.parse_log(RawLog {
+        topics: tx.logs[0]
+            .topics
+            .iter()
+            .map(|x| H256::from_slice(x.as_bytes()))
+            .collect(),
+        data: Vec::from(tx.logs[0].data.0.as_slice()),
+    }) {
+        Ok(p) => Ok(p),
+        Err(_) => Err(()),
+    }
+}
+
+pub async fn get_latest_block_num(web3: &Web3<WebSocket>) -> Result<u64, web3::Error> {
+    let block = web3
+        .eth()
+        .block(BlockId::Number(BlockNumber::Latest))
+        .await?;
+    let block = match block {
+        Some(b) => b,
+        None => return Err(web3::Error::Internal),
+    };
+    match block.number {
+        Some(n) => Ok(n.as_u64()),
+        None => Err(web3::Error::Internal),
+    }
+}
+
+pub async fn get_block(
+    block_num: u64,
+    web3: &Web3<WebSocket>,
+) -> Result<Block<Transaction>, web3::Error> {
+    let block_num: [u64; 1] = [block_num];
+    let block = web3
+        .eth()
+        .block_with_txs(BlockId::Number(BlockNumber::Number(web3::types::U64(
+            block_num,
+        ))))
+        .await;
+
+    let block = match block {
+        Ok(b) => b,
+        Err(e) => return Err(e),
+    };
+    match block {
+        Some(b) => Ok(b),
+        None => Err(web3::Error::Internal),
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,37 @@
-use web3::{
-    transports::WebSocket,
-    types::{Block, BlockId, BlockNumber, Transaction},
-    Web3,
-};
+mod helpers;
 
-struct OraculConf {
-    eth_api_url: String,
-    // contract_address: String,
-}
+use helpers::{get_block, get_event_logs, get_latest_block_num, OraculConf, TransferFraudReported};
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), web3::Error> {
     dotenvy::dotenv().expect("Failed to read .env file");
 
     let conf = OraculConf {
-        eth_api_url: std::env::var("ETH_API_URL").expect("env err"),
-        // contract_address: std::env::var("CONTRACT_ADDRESS").expect("env err"),
+        eth_api_url: env::var("ETH_API_URL").expect("env err"),
+        contract_address: {
+            let mut addr = env::var("CONTRACT_ADDRESS")
+                .expect("env err")
+                .to_lowercase();
+            if !addr.starts_with("0x") {
+                addr = format!("0x{}", addr);
+            }
+            addr
+        },
+        contract: {
+            let abi_bytes = tokio::fs::read(env::var("ABI_PATH").expect("env err (ABI_PATH)"))
+                .await
+                .expect("invalid hex");
+
+            let full_json_with_abi: serde_json::Value =
+                serde_json::from_slice(&abi_bytes).expect("parse json abi failed");
+
+            let x =
+                serde_json::to_vec(full_json_with_abi.get("abi").expect("create abi bytes err"))
+                    .expect("create abi err");
+
+            web3::ethabi::Contract::load(&*x).expect("load contract err")
+        },
     };
 
     let web3 = web3::Web3::new(web3::transports::WebSocket::new(&conf.eth_api_url).await?);
@@ -43,52 +59,58 @@ async fn main() -> Result<(), web3::Error> {
                 Err(_) => continue,
             };
 
-            println!("BLOCK: {:#?}", block);
-
             for tx in block.transactions {
-                println!("TX: {:#?}", tx);
+                if &format!(
+                    "0x{:x}",
+                    match tx.to {
+                        Some(to) => to,
+                        None => {
+                            continue;
+                        }
+                    }
+                ) != &conf.contract_address
+                {
+                    continue;
+                }
+
+                let tx_logs =
+                    match get_event_logs("TransferFraudReported", tx.hash, &web3, &conf.contract)
+                        .await
+                    {
+                        Ok(r) => r,
+                        Err(_) => continue,
+                    };
+
+                let mut report = TransferFraudReported {
+                    token_id: 0,
+                    decided: false,
+                    approved: false,
+                };
+
+                for log in tx_logs.params {
+                    match log.name.as_str() {
+                        "tokenId" => match log.value {
+                            web3::ethabi::Token::Uint(u) => report.token_id = u.as_u64(),
+                            _ => continue,
+                        },
+                        "decided" => match log.value {
+                            web3::ethabi::Token::Bool(b) => report.decided = b,
+                            _ => continue,
+                        },
+                        "approved" => match log.value {
+                            web3::ethabi::Token::Bool(b) => report.approved = b,
+                            _ => continue,
+                        },
+                        &_ => continue,
+                    }
+                }
+
+                println!("{:#?}", report);
             }
 
             old_block_num = new_block_num;
         }
 
         tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
-    }
-}
-
-async fn get_latest_block_num(web3: &Web3<WebSocket>) -> Result<u64, web3::Error> {
-    let block = web3
-        .eth()
-        .block(BlockId::Number(BlockNumber::Latest))
-        .await?;
-    let block = match block {
-        Some(b) => b,
-        None => return Err(web3::Error::Internal),
-    };
-    match block.number {
-        Some(n) => Ok(n.as_u64()),
-        None => return Err(web3::Error::Internal),
-    }
-}
-
-async fn get_block(
-    block_num: u64,
-    web3: &Web3<WebSocket>,
-) -> Result<Block<Transaction>, web3::Error> {
-    let block_num: [u64; 1] = [block_num];
-    let block = web3
-        .eth()
-        .block_with_txs(BlockId::Number(BlockNumber::Number(web3::types::U64(
-            block_num,
-        ))))
-        .await;
-
-    let block = match block {
-        Ok(b) => b,
-        Err(e) => return Err(e),
-    };
-    match block {
-        Some(b) => Ok(b),
-        None => return Err(web3::Error::Internal),
     }
 }
