9673b3fd6ce4adc8ede393275657f3d5a1145cbb
George Kobunov <ancient.gekob@gmail.com>
2022-11-07 14:56:56 +0300

    change drop shadow to box shadow

--- frontend/src/app/components/MarketCard/BasicCard.tsx
+++ frontend/src/app/components/MarketCard/BasicCard.tsx
@@ -6,7 +6,7 @@ export const BasicCard = styled('div', {
   borderRadius: '$3',
   position: 'relative',
   overflow: 'hidden',
-  filter: 'drop-shadow(0px 4px 15px rgba(19, 19, 45, 0.1))',
+  boxShadow: '0px 4px 15px rgba(19, 19, 45, 0.1)',
   '&:hover': {
     filter: 'drop-shadow(0px 12px 25px rgba(19, 19, 45, 0.35))'
   }
