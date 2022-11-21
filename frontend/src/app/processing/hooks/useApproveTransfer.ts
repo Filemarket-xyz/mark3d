ed6a0cee86f80a56c95c8ac9c81ccc2776827dd3
santalov <santalov.stas@gmail.com>
2022-11-21 22:23:55 +0300

    useApproveTransfer

--- frontend/src/app/processing/hooks/useApproveTransfer.ts
+++ frontend/src/app/processing/hooks/useApproveTransfer.ts
@@ -1,31 +1,37 @@
 import { useCollectionContract } from './useCollectionContract'
 import { useStatusState } from '../../hooks'
-import { ContractReceipt } from 'ethers'
+import { BigNumber, ContractReceipt, utils } from 'ethers'
 import { useCallback } from 'react'
 import { useHiddenFileProcessorFactory } from './useHiddenFileProcessorFactory'
 import { assertContract, assertSigner } from '../utils/assert'
 import { mark3dConfig } from '../../config/mark3d'
-import { Transfer, TransferStatus } from '../../../swagger/Api'
-import { useTransaction } from 'wagmi'
+import { Transfer } from '../../../swagger/Api'
+import * as assert from 'assert'
 
 export function useApproveTransfer(collectionAddress?: string, tokenId?: string, transfer?: Transfer) {
   const { contract, signer } = useCollectionContract(collectionAddress)
   const { statuses, wrapPromise } = useStatusState<ContractReceipt>()
   const factory = useHiddenFileProcessorFactory()
 
-  const setPublicKeyTxId = transfer?.statuses?.find(status => status.status === TransferStatus.PublicKeySet)?.txId
-
-  const setPublicKeyTx = useTransaction({
-    hash: setPublicKeyTxId as `0x${string}` | undefined
-  })
+  const publicKeyStr = transfer?.publicKey
 
   const approveTransfer = useCallback(wrapPromise(async () => {
     assertContract(contract, mark3dConfig.collectionToken.name)
     assertSigner(signer)
-    if (collectionAddress && tokenId) {
-      const owner = await factory.getOwner({ collectionAddress, tokenId })
-    } else {
-      throw Error('collectionAddress or tokenId is not provided')
-    }
-  }), [contract, wrapPromise])
+    assert(collectionAddress, 'collection address not provided')
+    assert(tokenId, 'tokenId is not provided')
+    assert(publicKeyStr, 'publicKey was not set (or transfer object is null)')
+    const owner = await factory.getOwner({ collectionAddress, tokenId })
+    const encryptedAESPassword = await owner.prepareFileAESKeyForBuyer(publicKeyStr)
+    const res = await contract.approveTransfer(
+      BigNumber.from(tokenId),
+      utils.hexlify(encryptedAESPassword) as `0x${string}`
+    )
+    return await res.wait()
+  }), [contract, signer, wrapPromise, publicKeyStr])
+
+  return {
+    ...statuses,
+    approveTransfer
+  }
 }
