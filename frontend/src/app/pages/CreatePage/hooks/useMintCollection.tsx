3d8a412dfed7e15db79ce8de57d993ecdf09368b
George Kobunov <ancient.gekob@gmail.com>
2022-11-22 17:11:41 +0300

    implement custom hook for handling minting collection

---
+++ frontend/src/app/pages/CreatePage/hooks/useMintCollection.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { useAfterDidMountEffect } from '../../../hooks/useDidMountEffect'
+import {
+  useCreateCollection,
+  CreateCollectionForm as FormDataToTransfer
+} from '../../../processing/hooks'
+import { CreateCollectionForm } from '../CreateCollectionPage'
+
+const convertFileListToFile = (file: FileList): File | undefined => {
+  if (!file || !file.length) return
+  const fileExtension = file[0].type.split('/').pop()
+
+  return new File([file[0]], `logo.${fileExtension}`, { type: file[0].type })
+}
+
+const convertFormDataToCollectionDTO = (
+  form: CreateCollectionForm
+): FormDataToTransfer => {
+  return {
+    ...form,
+    name: form.name,
+    image: convertFileListToFile(form.image)
+  }
+}
+
+/** This hook provides methods to mint collection from given form */
+export const useMintCollection = () => {
+  const [formToTransfer, setFormToTransfer] = useState<FormDataToTransfer>({
+    description: '',
+    image: undefined,
+    name: '',
+    symbol: ''
+  })
+
+  const {
+    createCollection,
+    statuses: { error, isLoading, result }
+  } = useCreateCollection(formToTransfer)
+
+  useAfterDidMountEffect(() => {
+    void createCollection()
+  }, [formToTransfer])
+
+  return {
+    error,
+    isLoading,
+    result,
+    mintCollection: (form: CreateCollectionForm) => {
+      setFormToTransfer(convertFormDataToCollectionDTO(form))
+    }
+  }
+}
