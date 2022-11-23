7146cdb29a30fc4639895095fe060c03b07c2292
George Kobunov <ancient.gekob@gmail.com>
2022-11-23 22:32:31 +0300

    now we fetch data about nfts in nfts token

--- frontend/src/app/pages/router.tsx
+++ frontend/src/app/pages/router.tsx
@@ -35,7 +35,7 @@ const routes: RouteObject[] = [
     children: profileRoutes
   },
   {
-    path: 'collection/:id',
+    path: 'collection/:collectionId',
     element: <CollectionPage />,
     children: collectionPageRoutes
   },
