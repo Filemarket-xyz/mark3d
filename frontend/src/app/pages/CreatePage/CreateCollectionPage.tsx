fe7be95b7acddbd4bf0921757ecb5cada0ea40bc
George Kobunov <ancient.gekob@gmail.com>
2022-11-23 14:58:52 +0300

    remove unnecessary line

--- frontend/src/app/pages/CreatePage/CreateCollectionPage.tsx
+++ frontend/src/app/pages/CreatePage/CreateCollectionPage.tsx
@@ -121,7 +121,6 @@ export default function CreateCollectionPage() {
     register,
     handleSubmit,
     formState: { isValid },
-    getFieldState,
     getValues
   } = useForm<CreateCollectionForm>()
 
