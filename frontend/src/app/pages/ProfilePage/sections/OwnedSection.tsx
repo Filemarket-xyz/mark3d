5b02202fabaa469e79a3f664bc7ab56e37ff2985
George Kobunov <ancient.gekob@gmail.com>
2022-11-24 20:22:09 +0300

    created separate component for cards placeholder

--- frontend/src/app/pages/ProfilePage/sections/OwnedSection.tsx
+++ frontend/src/app/pages/ProfilePage/sections/OwnedSection.tsx
@@ -1,26 +1,12 @@
-import { Skeleton } from '@mui/material'
 import { toJS } from 'mobx'
 import { observer } from 'mobx-react-lite'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { CardsPlaceholder } from '../../../components/CardsPlaceholder/CardsPlaceholder'
 import NFTCard, { NFTCardProps } from '../../../components/MarketCard/NFTCard'
 import { useCollectionAndTokenListStore } from '../../../hooks'
 import { getIHttpLinkFromIpfsString } from '../../CollectionPage/sections/NftSection'
 import { CardsContainer } from '../../MarketPage/NftSection'
-
-const CardsPlaceholder = () => (
-  <>
-    {new Array<number>(5).fill(0).map((_, i) => (
-      <Skeleton
-        sx={{ borderRadius: '16px' }}
-        variant='rectangular'
-        width={259}
-        height={324}
-        key={i}
-      />
-    ))}
-  </>
-)
 
 export const OwnedSection = observer(() => {
   const { profileId } = useParams<{ profileId: `0x${string}` }>()
@@ -50,7 +36,7 @@ export const OwnedSection = observer(() => {
   return (
     <CardsContainer>
       {isLoading ? (
-        <CardsPlaceholder />
+        <CardsPlaceholder cardsAmount={5}/>
       ) : (
         cards.map((card, i) => <NFTCard {...card} key={i} />)
       )}
