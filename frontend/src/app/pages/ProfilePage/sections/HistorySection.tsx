93ccce0888d74b268ba16521e5a2482d8d914d78
George Kobunov <ancient.gekob@gmail.com>
2022-12-07 16:03:05 +0300

    removed unused mocks

--- frontend/src/app/pages/ProfilePage/sections/HistorySection.tsx
+++ frontend/src/app/pages/ProfilePage/sections/HistorySection.tsx
@@ -1,7 +1,7 @@
 import { styled } from '../../../../styles'
 import { TableBody } from '../../ExplorerPage/components/Table/Table'
 import { RowCell } from '../../ExplorerPage/components/TableRow/TableRow'
-import { ITableColumn, ITableRow } from '../../../components/Table/TableBuilder'
+import { ITableColumn } from '../../../components/Table/TableBuilder'
 import { Button } from '../../../UIkit'
 import openLinkIcon from '../img/open-link-icon.svg'
 import { HistoryTableBuilder } from './HistoryTableBuilder'
@@ -9,8 +9,6 @@ import { observer } from 'mobx-react-lite'
 import { useTransfersHistory } from '../../../hooks/useTransfersHistory'
 import { useParams } from 'react-router-dom'
 import { Params } from '../../../utils/router/Params'
-import Badge from '../../../components/Badge/Badge'
-import { gradientPlaceholderImg } from '../../../components/Placeholder/GradientPlaceholder'
 
 export const Wrapper = styled(TableBody, {
   gap: '$2',
@@ -60,57 +58,6 @@ export const EmptyTablePlaceholder = styled('div', {
   color: '$gray500',
   paddingBottom: '$3'
 })
-
-// mock rows
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const rows: ITableRow[] = [
-  {
-    cells: [
-      { columnName: 'Event', value: 'Listing' },
-      {
-        columnName: 'Object',
-        value: (
-          <Badge
-            image={{
-              borderRadius: 'roundedSquare',
-              url: gradientPlaceholderImg
-            }}
-            content={{ value: '0x213...1234', title: 'VR glasses' }}
-            small
-            wrapperProps={{ css: { padding: 0 } }}
-          />
-        ),
-        cellAttributes: {
-          css: {
-            flexGrow: 1.5
-          }
-        }
-      },
-      { columnName: 'From', value: 'Sale' },
-      { columnName: 'To', value: 'Sale' },
-      { columnName: 'Price', value: 'Sale' },
-      {
-        columnName: 'Date',
-        value: 'Some date',
-        cellAttributes: {
-          css: {
-            flexGrow: 1.5
-          }
-        }
-      }
-    ]
-  },
-  {
-    cells: [
-      { columnName: 'Event', value: 'Sale' },
-      { columnName: 'Object', value: 'Sale' },
-      { columnName: 'From', value: 'Sale' },
-      { columnName: 'To', value: 'Sale' },
-      { columnName: 'Price', value: 'Sale' },
-      { columnName: 'Date', value: 'Some date' }
-    ]
-  }
-]
 
 export const HistorySection = observer(() => {
   const { profileAddress } = useParams<Params>()
