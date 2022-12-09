e09d579ced5b8e820d932cb790ee88c242f2020b
George Kobunov <ancient.gekob@gmail.com>
2022-12-10 00:04:57 +0300

    fixed prod build

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -8,28 +8,6 @@ declare module '*.module.css' {
   export = classNames
 }
 
-declare module '@lighthouse-web3/sdk' {
-  async function upload(
-    // an event fired by <input type="file"> element
-    e:
-    | React.ChangeEvent<HTMLInputElement>
-    | { target: { files: File[] }, persist: () => void },
-    // token, obtained via lighthouse api
-    accessToken: string,
-    uploadProgressCallback: (progress: {
-      progress: number
-      total: number
-      uploaded: number
-    }) => void
-  ): Promise<{
-    data: {
-      Name: string
-      Size: number
-      Hash: string
-    }
-  }>
-}
-
 declare global {
   interface ModelViewerProps
     extends React.DetailedHTMLProps<
