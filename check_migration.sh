fb1f230894cc0af2504360f3c2598b679f5cda80
Oleg Shatniuk <o.shatnuk@gmail.com>
2023-01-24 19:33:08 +0300

    fix check migration script

--- check_migration.sh
+++ check_migration.sh
@@ -1,10 +1,10 @@
 while true; do
-    if [ $(docker inspect mark3d-indexer-postgres-migration-1 --format='{{.State.Status}}') == "exited" ]; then
-        echo $(docker inspect mark3d-indexer-postgres-migration-1 --format='{{.State.Status}}')
+    if [ $(docker inspect file-market-indexer-postgres-migration-1 --format='{{.State.Status}}') == "exited" ]; then
+        echo $(docker inspect file-market-indexer-postgres-migration-1 --format='{{.State.Status}}')
         break
     fi
     done
 
-echo $(docker inspect mark3d-indexer-postgres-migration-1 --format='{{.State.ExitCode}}')
-docker logs mark3d-indexer-postgres-migration-1
-exit $(docker inspect mark3d-indexer-postgres-migration-1 --format='{{.State.ExitCode}}')
+echo $(docker inspect file-market-indexer-postgres-migration-1 --format='{{.State.ExitCode}}')
+docker logs file-market-indexer-postgres-migration-1
+exit $(docker inspect file-market-indexer-postgres-migration-1 --format='{{.State.ExitCode}}')
