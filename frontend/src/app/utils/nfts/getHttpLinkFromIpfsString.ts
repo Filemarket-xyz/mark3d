9d4ced3257e5f726eb637ddbe0d57867af0d4ea1
santalov <santalov.stas@gmail.com>
2023-02-01 13:44:18 +0300

    change ipfs gateway

--- frontend/src/app/utils/nfts/getHttpLinkFromIpfsString.ts
+++ frontend/src/app/utils/nfts/getHttpLinkFromIpfsString.ts
@@ -7,5 +7,5 @@ export const getHttpLinkFromIpfsString = (ipfs: string) => {
   if (!ipfs) return ''
   const cidWithFilePath = getIpfsCidWithFilePath(ipfs)
 
-  return `https://mark3d.xyz/ipfs/${cidWithFilePath}`
+  return `https://gateway.lighthouse.storage/ipfs/${cidWithFilePath}`
 }
