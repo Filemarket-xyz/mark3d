85ea32ad7a33136a9c20e54965722f35466b88bf
w8noot <ynifor63@gmail.com>
2023-04-05 23:36:53 +0500

    decryption logic for new format and algorithm (AES256CBC)

--- backend/src/helpers.rs
+++ backend/src/helpers.rs
@@ -1,29 +1,29 @@
 use crate::structs::{CollectionCreation, File, FraudReported, TransferFraudReported};
 use aes::{
-    cipher::{BlockDecrypt, KeyInit},
+    cipher::{block_padding::Pkcs7, BlockDecrypt, BlockDecryptMut, KeyInit, KeyIvInit},
     Aes256,
 };
 use generic_array::GenericArray;
 use openssl::{
-    hash::MessageDigest,
-    pkcs5::pbkdf2_hmac,
     pkey::Private,
     rsa::{Padding, Rsa},
 };
 use redis::{aio::Connection, AsyncCommands};
 use secp256k1::SecretKey;
-use sha2::{Digest, Sha256};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sha2::{Digest, Sha256, Sha512};
 use std::{collections::HashSet, env, error::Error, str::FromStr};
 use web3::{
+    contract::Options,
     ethabi::{Contract, Log, RawLog},
+    signing::Key,
     transports::Http,
-    types::{Block, BlockId, BlockNumber, Transaction, TransactionReceipt, H160, H256, Address, Index},
-    Web3,
-    signing::Key, contract::Options,
-    Transport
+    types::{
+        Address, Block, BlockId, BlockNumber, Index, Transaction, TransactionReceipt, H160, H256,
+    },
+    Transport, Web3,
 };
-use serde_json::Value;
-use serde::{Deserialize, Serialize};
 
 pub async fn add_in_set(
     con: &mut Connection,
@@ -51,10 +51,7 @@ pub async fn get_contract(env_var: &str) -> web3::ethabi::Contract {
     web3::ethabi::Contract::load(&*x).expect("load contract err")
 }
 
-async fn get_tx_receipt(
-    hash: H256,
-    web3: &Web3<Http>,
-) -> Result<TransactionReceipt, web3::Error> {
+async fn get_tx_receipt(hash: H256, web3: &Web3<Http>) -> Result<TransactionReceipt, web3::Error> {
     if let Some(tx) = match web3.eth().transaction_receipt(hash).await {
         Ok(tx) => tx,
         Err(e) => return Err(e),
@@ -130,7 +127,7 @@ pub async fn get_events(
                 .collect(),
             data: Vec::from(tx.logs[i].data.0.as_slice()),
         }) {
-            left_events-=1;
+            left_events -= 1;
             t_f_r_log = log;
         }
 
@@ -142,7 +139,7 @@ pub async fn get_events(
                 .collect(),
             data: Vec::from(tx.logs[i].data.0.as_slice()),
         }) {
-            left_events-=1;
+            left_events -= 1;
             f_r_log = log;
         }
     }
@@ -243,6 +240,49 @@ pub fn decrypt_file(file: &[u8], key_bytes: &[u8]) -> Result<bool, web3::Error> 
     Ok(result == hash)
 }
 
+////
+// AES-256_CBC
+fn decrypt_aes256cbc(data: &[u8], key_bytes: &[u8], iv: &[u8]) -> Result<Vec<u8>, web3::Error> {
+    type Aes256CbcDec = cbc::Decryptor<aes::Aes256>;
+
+    let cipher = match Aes256CbcDec::new_from_slices(&key_bytes, &iv) {
+        Ok(c) => c,
+        Err(e) => return Err(web3::Error::Decoder(format!("parse key failed: {e}"))),
+    };
+
+    let mut dec_buffer = vec![0u8; data.len()];
+    dec_buffer[..data.len()].copy_from_slice(&data);
+
+    let decrypted = match cipher.decrypt_padded_b2b_mut::<Pkcs7>(&data, &mut dec_buffer) {
+        Ok(d) => d.to_vec(),
+        Err(e) => return Err(web3::Error::Decoder(format!("decryption failed: {e}"))),
+    };
+
+    Ok(decrypted)
+}
+
+pub fn verify_file_integrity(file: &[u8], key_bytes: &[u8]) -> Result<bool, web3::Error> {
+    // iv(16b) | SHA-512(64b) | encryptedData
+    let iv_padding = 16;
+    let hash_padding = 64;
+
+    let iv = &file[..iv_padding];
+    let hash = &file[iv_padding..hash_padding + iv_padding];
+    let encrypted = &file[hash_padding + iv_padding..];
+
+    let decrypted = match decrypt_aes256cbc(&encrypted, &key_bytes, &iv) {
+        Ok(d) => d,
+        Err(e) => return Err(web3::Error::Decoder(format!("decryption failed: {e}"))),
+    };
+
+    let mut hasher = Sha512::new();
+    hasher.update(&decrypted);
+    let res = hasher.finalize();
+    let result = res.as_slice();
+
+    Ok(result == hash)
+}
+
 /// Description of a Transaction, pending or in the chain.
 #[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
 pub struct TransactionNoExtraData {
@@ -285,15 +325,23 @@ pub async fn get_latest_block_num(web3: &Web3<Http>) -> Result<u64, web3::Error>
     }
 }
 
-
-pub async fn get_latest_block_num_no_extra(transport: &web3::transports::Http) -> Result<u64, web3::Error> {
-    let result = transport.execute("eth_getBlockByNumber",
-     vec![Value::String(String::from_str("latest").unwrap()), Value::Bool(false)]).await?;
+pub async fn get_latest_block_num_no_extra(
+    transport: &web3::transports::Http,
+) -> Result<u64, web3::Error> {
+    let result = transport
+        .execute(
+            "eth_getBlockByNumber",
+            vec![
+                Value::String(String::from_str("latest").unwrap()),
+                Value::Bool(false),
+            ],
+        )
+        .await?;
     match result.get("error") {
         Some(value) => {
             log::error!("failed to get latest block number: {}", value.to_string());
-            return Err(web3::Error::Internal)
-        },
+            return Err(web3::Error::Internal);
+        }
         None => (),
     }
     let block: BlockNoExtraData<web3::types::H256> = match serde_json::from_value(result) {
@@ -301,7 +349,7 @@ pub async fn get_latest_block_num_no_extra(transport: &web3::transports::Http) -
         Err(e) => {
             log::error!("failed to get latest block number: {}", e);
             return Err(web3::Error::Internal);
-        },
+        }
     };
 
     match block.number {
@@ -334,19 +382,26 @@ pub async fn get_block(
 
 pub async fn get_block_no_extra(
     block_num: u64,
-    transport: &web3::transports::Http
+    transport: &web3::transports::Http,
 ) -> Result<BlockNoExtraData<TransactionNoExtraData>, web3::Error> {
     let block_num: [u64; 1] = [block_num];
-    let result = transport.execute("eth_getBlockByNumber",
-    vec![serde_json::to_value(
-        BlockId::Number(
-            BlockNumber::Number(web3::types::U64(block_num)),
-        )).unwrap(), Value::Bool(true)]).await?;
+    let result = transport
+        .execute(
+            "eth_getBlockByNumber",
+            vec![
+                serde_json::to_value(BlockId::Number(BlockNumber::Number(web3::types::U64(
+                    block_num,
+                ))))
+                .unwrap(),
+                Value::Bool(true),
+            ],
+        )
+        .await?;
     match result.get("error") {
         Some(value) => {
             log::error!("failed to get block: {}", value.to_string());
-            return Err(web3::Error::Internal)
-        },
+            return Err(web3::Error::Internal);
+        }
         None => (),
     }
     let block: BlockNoExtraData<TransactionNoExtraData> = match serde_json::from_value(result) {
@@ -354,7 +409,7 @@ pub async fn get_block_no_extra(
         Err(e) => {
             log::error!("failed to get latest block number: {}", e);
             return Err(web3::Error::Internal);
-        },
+        }
     };
     Ok(block)
 }
@@ -371,38 +426,46 @@ pub async fn call_late_decision(
         web3::ethabi::Token::Uint(report.token_id),
         web3::ethabi::Token::Bool(approved),
     );
-    let gas = match contract.estimate_gas(
-        "lateDecision", 
-        params,
-        key.address(),
-        web3::contract::Options::default())
-        .await {
-            Ok(g) => g,
-            Err(_) => return Err(web3::Error::Internal),
-        };
-    let max_fee_per_gas_result = transport.execute("eth_maxPriorityFeePerGas", vec![]).await?;
+    let gas = match contract
+        .estimate_gas(
+            "lateDecision",
+            params,
+            key.address(),
+            web3::contract::Options::default(),
+        )
+        .await
+    {
+        Ok(g) => g,
+        Err(_) => return Err(web3::Error::Internal),
+    };
+    let max_fee_per_gas_result = transport
+        .execute("eth_maxPriorityFeePerGas", vec![])
+        .await?;
     match max_fee_per_gas_result.get("error") {
         Some(value) => {
-            log::error!("failed to get max priority fee per gas: {}", value.to_string());
-            return Err(web3::Error::Internal)
-        },
+            log::error!(
+                "failed to get max priority fee per gas: {}",
+                value.to_string()
+            );
+            return Err(web3::Error::Internal);
+        }
         None => (),
     };
-    let max_priority_fee_per_gas: web3::types::U256 = match serde_json::from_value(max_fee_per_gas_result) {
-        Ok(b) => b,
-        Err(e) => {
-            log::error!("failed to parse max priority fee per gas: {}", e);
-            return Err(web3::Error::Internal);
-        },
-    };
-    
-    
-    let opts  = Options { 
+    let max_priority_fee_per_gas: web3::types::U256 =
+        match serde_json::from_value(max_fee_per_gas_result) {
+            Ok(b) => b,
+            Err(e) => {
+                log::error!("failed to parse max priority fee per gas: {}", e);
+                return Err(web3::Error::Internal);
+            }
+        };
+
+    let opts = Options {
         gas: Some(gas),
         transaction_type: Some(2.into()),
         max_priority_fee_per_gas: Some(max_priority_fee_per_gas),
         ..Default::default()
-     };
+    };
     contract
         .signed_call(
             "lateDecision",
@@ -415,4 +478,51 @@ pub async fn call_late_decision(
             key,
         )
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decrypt_aes256cbc() {
+        let data = [
+            0xE0, 0xC7, 0xCF, 0x54, 0xA3, 0xC2, 0xA6, 0xA2, 0x25, 0x2B, 0xD5, 0x4E, 0xA9, 0xFD,
+            0x8D, 0xF7,
+        ]
+        .to_vec();
+        let key = *b"12345678901234567890123456789012";
+        let iv = *b"1234567890123456";
+        let expected = *b"hello world";
+
+        let result = decrypt_aes256cbc(&data, &key, &iv).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_verify_file_integrity() {
+        let key = [
+            0xd2, 0xf1, 0xd9, 0x6f, 0x9c, 0x4f, 0x52, 0x86, 0xee, 0x9c, 0xa3, 0x4c, 0xf8, 0x0b,
+            0x9b, 0xe8, 0x15, 0x45, 0xae, 0xbf, 0x71, 0x9b, 0xab, 0x08, 0x40, 0x0c, 0xc2, 0xb9,
+            0xf8, 0x01, 0xab, 0xcb,
+        ]
+        .to_vec(); // d2f1d96f9c4f5286ee9ca34cf80b9be81545aebf719bab08400cc2b9f801abcb
+        let data = [
+            0x16, 0x65, 0xc8, 0x5c, 0x10, 0x0a, 0xb0, 0xa6, 0xa1, 0xcf, 0x32, 0xd3, 0x3c, 0x9e,
+            0xdc, 0x64, 0x61, 0x57, 0x2c, 0xd0, 0xa9, 0xf6, 0x3f, 0x18, 0x67, 0x3a, 0x3c, 0x9d,
+            0x16, 0x22, 0xbd, 0x90, 0x7c, 0x97, 0xf5, 0xd1, 0x16, 0x50, 0x57, 0xda, 0xbc, 0xc6,
+            0xc6, 0x85, 0x6e, 0x30, 0xae, 0xb2, 0xa6, 0x0b, 0x37, 0x9c, 0xa5, 0xc5, 0xc9, 0xb2,
+            0xea, 0xc0, 0x3c, 0xab, 0xb3, 0xbe, 0xa9, 0x32, 0x22, 0x64, 0x59, 0xaf, 0xf9, 0x5a,
+            0x1d, 0xb4, 0x3a, 0x3d, 0x94, 0xa9, 0xc6, 0xb6, 0x65, 0xe9, 0x0f, 0x94, 0xeb, 0x5b,
+            0xb0, 0x19, 0x21, 0x36, 0x98, 0x35, 0x1c, 0x74, 0x59, 0x19, 0x22, 0x1a, 0xa5, 0xf1,
+            0xb5, 0x73, 0xea, 0x5d, 0x5a, 0x43, 0x53, 0xa8, 0x83, 0x03, 0xa2, 0xe4, 0x43, 0xab,
+            0x58, 0x88, 0x9b, 0x14, 0x4c, 0x16, 0x41, 0x4b, 0x94, 0xb1, 0xc0, 0x55, 0x98, 0x24,
+            0x17, 0x85,
+        ]
+        .to_vec(); // 1665c85c100ab0a6a1cf32d33c9edc6461572cd0a9f63f18673a3c9d1622bd907c97f5d1165057dabcc6c6856e30aeb2a60b379ca5c5c9b2eac03cabb3bea932226459aff95a1db43a3d94a9c6b665e90f94eb5bb019213698351c745919221aa5f1b573ea5d5a4353a88303a2e443ab58889b144c16414b94b1c05598241785
+
+        let ok = verify_file_integrity(&data, &key).unwrap();
+
+        assert!(ok);
+    }
 }
