f4468afefad21e57f5d1dbcaefd8f46f3b738a52
alik <colinmilton1@gmail.com>
2023-04-22 22:10:56 +0300

    HiddenFileOwner update

--- packages/frontend/src/app/processing/HiddenFileOwner/HiddenFileOwner.ts
+++ packages/frontend/src/app/processing/HiddenFileOwner/HiddenFileOwner.ts
@@ -1,38 +1,48 @@
+import { EftAesDerivationFunction, RsaPublicKey } from '../../../../../crypto/src/lib/types'
+import { buf2Hex } from '../../../../../crypto/src/lib/utils'
+import { fileMarketCrypto } from '../FileMarketCrypto'
+import { DecryptResult, FileMeta } from '../types'
 import { IHiddenFileOwner } from './IHiddenFileOwner'
-import { IStatefulCryptoProvider } from '../StatefulCryptoProvider'
-import { AESEncoding, CryptoMessage, DecryptResult, FileMeta, RSAPublicKey } from '../types'
-import { NoAESKeyToSendBuyerError } from './errors'
-import { encryptRSA } from '../utils'
 
 export class HiddenFileOwner implements IHiddenFileOwner {
-  constructor(
-    public readonly cryptoProvider: IStatefulCryptoProvider,
-    public readonly surrogateId: string
-  ) {
-  }
+  constructor(public readonly surrogateId: string) {}
 
-  async decryptFile(encryptedFileData: CryptoMessage, meta?: FileMeta): Promise<DecryptResult<File>> {
-    const result = await this.cryptoProvider.decryptAES(encryptedFileData)
-    if (result.ok) {
+  async decryptFile(
+    encryptedFileData: ArrayBuffer,
+    meta: FileMeta | undefined,
+    ...args: Parameters<EftAesDerivationFunction>
+  ): Promise<DecryptResult<File>> {
+    const { key } = await fileMarketCrypto.eftAesDerivation(...args)
+
+    try {
+      const decryptedFile = await fileMarketCrypto.aesDecrypt(encryptedFileData, key)
+
       return {
         ok: true,
-        result: new File([result.result], meta?.name || 'hidden_file', { type: meta?.type })
+        result: new File([decryptedFile], meta?.name || 'hidden_file', { type: meta?.type })
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error: `Decrypt failed: ${error}`
       }
     }
-    return result
   }
 
-  async encryptFile(file: File): Promise<CryptoMessage> {
-    await this.cryptoProvider.genAESKey()
-    const fileData = new Uint8Array(await file.arrayBuffer())
-    return await this.cryptoProvider.encryptAES(fileData)
+  async encryptFile(file: File, ...args: Parameters<EftAesDerivationFunction>): Promise<ArrayBuffer> {
+    const arrayBuffer = await file.arrayBuffer()
+    const aesKeyAndIv = await fileMarketCrypto.eftAesDerivation(...args)
+
+    return fileMarketCrypto.aesEncrypt(arrayBuffer, aesKeyAndIv)
   }
 
-  async prepareFileAESKeyForBuyer(publicKey: RSAPublicKey): Promise<CryptoMessage> {
-    const key = await this.cryptoProvider.getAESKey()
-    if (!key) {
-      throw new NoAESKeyToSendBuyerError(this.surrogateId)
-    }
-    return await encryptRSA(Buffer.from(key, AESEncoding), publicKey)
+  async prepareFileAESKeyForBuyer(
+    publicKey: RsaPublicKey,
+    ...args: Parameters<EftAesDerivationFunction>
+  ): Promise<`0x${string}`> {
+    const { key } = await fileMarketCrypto.eftAesDerivation(...args)
+    const encryptedPublicKey = await fileMarketCrypto.rsaEncrypt(key, publicKey)
+
+    return `0x${buf2Hex(encryptedPublicKey)}`
   }
 }
--- packages/frontend/src/app/processing/HiddenFileOwner/IHiddenFileOwner.ts
+++ packages/frontend/src/app/processing/HiddenFileOwner/IHiddenFileOwner.ts
@@ -1,5 +1,7 @@
-import { CryptoMessage, DecryptResult, FileMeta, RSAPublicKey } from '../types'
+/* eslint-disable max-len */
+import { EftAesDerivationFunction, RsaPublicKey } from '../../../../../crypto/src/lib/types'
 import { IHiddenFileBase } from '../HiddenFileBase'
+import { DecryptResult, FileMeta } from '../types'
 
 /**
  * Used to mint and sell NFT
@@ -9,22 +11,21 @@ export interface IHiddenFileOwner extends IHiddenFileBase {
   /**
    * Generates AES key, saves it and encrypts the file.
    * @param file
-   * @returns {@link CryptoMessage} Encrypted file data
+   * @returns {@link ArrayBuffer} Encrypted file data
    */
-  encryptFile: (file: File) => Promise<CryptoMessage>
+  encryptFile: (file: File, ...args: Parameters<EftAesDerivationFunction>) => Promise<ArrayBuffer>
 
   /**
    * Decrypts the file if AES key exists.
-   * @throws {@link NoAESKeyToDecrypt}
    * @param file
    */
-  decryptFile: (encryptedFileData: CryptoMessage, meta?: FileMeta) => Promise<DecryptResult<File>>
+  decryptFile: (encryptedFileData: ArrayBuffer, meta: FileMeta | undefined, ...args: Parameters<EftAesDerivationFunction>) => Promise<DecryptResult<File>>
 
   /**
    * Encrypts stored AES key (AES key used to encrypt file) with provided publicKey.
    * Public key is not saved, it is used immediately.
    * @param publicKey
    */
-  prepareFileAESKeyForBuyer: (publicKey: RSAPublicKey) => Promise<CryptoMessage>
+  prepareFileAESKeyForBuyer: (publicKey: RsaPublicKey, ...args: Parameters<EftAesDerivationFunction>) => Promise<`0x${string}`>
 
 }
