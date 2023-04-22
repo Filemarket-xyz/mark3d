d58d0f1ad97ae71a7828b8775aa25ddaf413935c
alik <colinmilton1@gmail.com>
2023-04-22 22:27:20 +0300

    useSetPublicKey update

--- packages/frontend/src/app/processing/nft-interaction/useSetPublicKey.ts
+++ packages/frontend/src/app/processing/nft-interaction/useSetPublicKey.ts
@@ -1,13 +1,17 @@
-import { TokenFullId } from '../types'
-import { useStatusState } from '../../hooks'
-import { BigNumber, ContractReceipt } from 'ethers'
-import { useHiddenFileProcessorFactory } from '../HiddenFileProcessorFactory'
-import { useCallback } from 'react'
 import assert from 'assert'
-import { assertContract, assertSigner } from '../utils/assert'
-import { mark3dConfig } from '../../config/mark3d'
-import { useCollectionContract } from '../contracts'
+import { BigNumber, ContractReceipt } from 'ethers'
+import { useCallback } from 'react'
 import { useAccount } from 'wagmi'
+
+import { str2ab } from '../../../../../crypto/src/lib/utils'
+import { mark3dConfig } from '../../config/mark3d'
+import { useStatusState } from '../../hooks'
+import { useCollectionContract } from '../contracts'
+import { useHiddenFileProcessorFactory } from '../HiddenFileProcessorFactory'
+import { useSeed } from '../SeedProvider/useSeed'
+import { TokenFullId } from '../types'
+import { globalSaltMock } from '../utils'
+import { assertContract, assertSigner } from '../utils/assert'
 
 /**
  * Sets public key in a transfer process
@@ -17,25 +21,41 @@ import { useAccount } from 'wagmi'
 export function useSetPublicKey({ collectionAddress, tokenId }: Partial<TokenFullId> = {}) {
   const { contract, signer } = useCollectionContract(collectionAddress)
   const { address } = useAccount()
+  const seed = useSeed(address)
   const { wrapPromise, statuses } = useStatusState<ContractReceipt>()
   const factory = useHiddenFileProcessorFactory()
+
   const setPublicKey = useCallback(wrapPromise(async () => {
     assert(collectionAddress, 'collectionAddress is not provided')
     assert(tokenId, 'tokenId is not provided')
+    assert(seed, 'seed not found')
     assertContract(contract, mark3dConfig.exchangeToken.name)
     assertSigner(signer)
     assert(address, 'need to connect wallet')
+
     const tokenFullId = { collectionAddress, tokenId }
+    const tokenIdBN = BigNumber.from(tokenId)
+    const transferCountBN = await contract.transferCounts(tokenIdBN)
     const buyer = await factory.getBuyer(address, tokenFullId)
     await factory.registerTokenFullId(address, buyer, tokenFullId)
-    const publicKey = await buyer.initBuy()
+    const publicKey = await buyer.initBuy(
+      seed,
+      globalSaltMock,
+      str2ab(collectionAddress),
+      +tokenId,
+      transferCountBN.toNumber()
+    )
     console.log('setTransferPublicKey', 'tokenId', tokenId, 'publicKey', publicKey)
-    const result = await contract.setTransferPublicKey(
-      BigNumber.from(tokenId),
-      publicKey as `0x${string}`,
+
+    const tx = await contract.setTransferPublicKey(
+      tokenIdBN,
+      publicKey,
+      transferCountBN,
       { gasPrice: mark3dConfig.gasPrice }
     )
-    return await result.wait()
-  }), [contract, signer, address, collectionAddress, tokenId])
+
+    return tx.wait()
+  }), [contract, signer, address, seed, collectionAddress, tokenId])
+
   return { ...statuses, setPublicKey }
 }
