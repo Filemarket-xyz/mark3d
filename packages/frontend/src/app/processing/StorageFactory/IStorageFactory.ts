26f526e2c233ab0d0b831b71fd233094bc4a426a
alik <colinmilton1@gmail.com>
2023-04-25 17:55:39 +0300

    TokenIdStorage removed

--- packages/frontend/src/app/processing/StorageFactory/IStorageFactory.ts
+++ packages/frontend/src/app/processing/StorageFactory/IStorageFactory.ts
@@ -2,14 +2,12 @@
  * Is responsible for creating StorageProvider, SecureStorageProvider, TokenIdStorage
  * for every account
  */
-import { IStorageProvider } from '../StorageProvider'
 import { ISecureStorage } from '../SecureStorage'
-import { ITokenIdStorage } from '../TokenIdStorage'
+import { IStorageProvider } from '../StorageProvider'
 
 export interface AllStorages {
   storageProvider: IStorageProvider
   secureStorage: ISecureStorage
-  tokenIdStorage: ITokenIdStorage
 }
 
 export interface IStorageFactory {
