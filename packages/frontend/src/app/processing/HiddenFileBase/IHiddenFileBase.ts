8a3386b1aae0e3602f04200415e53a2dcfc9cf31
alik <colinmilton1@gmail.com>
2023-05-02 01:24:06 +0300

    IHiddenFileBase update

--- packages/frontend/src/app/processing/HiddenFileBase/IHiddenFileBase.ts
+++ packages/frontend/src/app/processing/HiddenFileBase/IHiddenFileBase.ts
@@ -1,7 +1,11 @@
 import { FileMarketCrypto } from '../../../../../crypto/src'
+import { IBlockchainDataProvider } from '../BlockchainDataProvider'
 import { ISeedProvider } from '../SeedProvider'
 
 export interface IHiddenFileBase {
+  readonly collectionAddress: ArrayBuffer
+  readonly tokenId: number
   readonly crypto: FileMarketCrypto
   readonly seedProvider: ISeedProvider
+  readonly blockchainDataProvider: IBlockchainDataProvider
 }
