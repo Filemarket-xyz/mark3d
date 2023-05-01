9212394ef5f6df4340734487da44eedcbc75a380
w8noot <ynifor63@gmail.com>
2023-05-01 08:43:16 +0500

    fix migration

--- indexer/migrations/20230419200001_metadata.sql
+++ indexer/migrations/20230419200001_metadata.sql
@@ -4,18 +4,43 @@ SELECT 'up SQL query';
 -- +goose StatementEnd
 CREATE TABLE public.token_metadata
 (
-    id          BIGSERIAL    NOT NULL,
-    name        VARCHAR(255) NOT NULL,
-    description TEXT         NOT NULL,
-    image       TEXT         NOT NULL,
-    hidden_file TEXT         NOT NULL,
-    category    TEXT         NOT NULL,
-    subcategory TEXT         NOT NULL,
-    PRIMARY KEY (id)
+    id                 BIGSERIAL    NOT NULL,
+    collection_address CHAR(42)     NOT NULL,
+    token_id           VARCHAR(255) NOT NULL,
+    name               VARCHAR(255) NOT NULL,
+    description        TEXT         NOT NULL,
+    image              TEXT         NOT NULL,
+    hidden_file        TEXT         NOT NULL,
+    license            TEXT         NOT NULL,
+    license_url        TEXT         NOT NULL,
+    external_link      TEXT         NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT token_metadata_tokens_fkey
+        FOREIGN KEY (collection_address, token_id)
+            REFERENCES public.tokens (collection_address, token_id)
+            ON DELETE CASCADE
 );
 
 ALTER TABLE public.token_metadata
     OWNER TO indexer;
+
+CREATE TABLE public.hidden_file_metadata
+(
+    id          BIGSERIAL NOT NULL,
+    metadata_id BIGINT NOT NULL,
+    name        VARCHAR(255) NOT NULL,
+    type        VARCHAR(255) NOT NULL,
+    size        BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT hidden_file_metadata_token_metadata_fkey
+        FOREIGN KEY (metadata_id)
+            REFERENCES public.token_metadata (id)
+            ON DELETE CASCADE
+);
+
+ALTER TABLE public.hidden_file_metadata
+    OWNER TO indexer;
+
 
 CREATE TYPE public.property_type_enum AS ENUM ('attribute', 'ranking', 'stat');
 
@@ -25,6 +50,7 @@ CREATE TABLE public.token_metadata_properties (
     trait_type    TEXT      NOT NULL,
     display_type  TEXT      NOT NULL,
     value_type    TEXT      NOT NULL,
+    max_value     FLOAT8    NOT NULL,
     property_type public.property_type_enum NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT token_metadata_properties_metadata_fkey
@@ -34,6 +60,36 @@ CREATE TABLE public.token_metadata_properties (
 );
 
 ALTER TABLE public.token_metadata_properties
+    OWNER TO indexer;
+
+CREATE TABLE public.token_metadata_categories
+(
+    id BIGSERIAL NOT NULL,
+    metadata_id BIGINT NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT token_metadata_categories_token_metadata
+        FOREIGN KEY (metadata_id)
+            REFERENCES public.token_metadata (id)
+            ON DELETE CASCADE
+);
+
+ALTER TABLE public.token_metadata_categories
+    OWNER TO indexer;
+
+CREATE TABLE public.token_metadata_subcategories
+(
+    id BIGSERIAL NOT NULL,
+    metadata_id BIGINT NOT NULL,
+    subcategory VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT token_metadata_subcategories_token_metadata
+        FOREIGN KEY (metadata_id)
+            REFERENCES public.token_metadata (id)
+            ON DELETE CASCADE
+);
+
+ALTER TABLE public.token_metadata_subcategories
     OWNER TO indexer;
 
 CREATE TABLE public.token_metadata_tags (
@@ -50,11 +106,6 @@ CREATE TABLE public.token_metadata_tags (
 ALTER TABLE public.token_metadata_tags
     OWNER TO indexer;
 
-ALTER TABLE public.tokens ADD COLUMN metadata_id BIGINT;
-ALTER TABLE public.tokens
-    ADD FOREIGN KEY (metadata_id)
-    REFERENCES public.token_metadata(id);
-
 ALTER TABLE public.tokens DROP COLUMN name;
 ALTER TABLE public.tokens DROP COLUMN description;
 ALTER TABLE public.tokens DROP COLUMN image;
@@ -63,16 +114,13 @@ ALTER TABLE public.tokens DROP COLUMN hidden_file;
 -- +goose StatementBegin
 SELECT 'down SQL query';
 -- +goose StatementEnd
-ALTER TABLE public.tokens
-    DROP CONSTRAINT tokens_metadata_id_fkey;
-ALTER TABLE public.tokens
-    DROP COLUMN metadata_id;
-
+DROP TABLE public.hidden_file_metadata;
 DROP TABLE public.token_metadata_tags;
 DROP TABLE public.token_metadata_properties;
-DROP TABLE public.token_metadata;
-
 DROP TYPE public.property_type_enum;
+DROP TABLE public.token_metadata_categories;
+DROP TABLE public.token_metadata_subcategories;
+DROP TABLE public.token_metadata;
 
 ALTER TABLE public.tokens
     ADD COLUMN name VARCHAR(255);
