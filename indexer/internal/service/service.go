95abedaec8b46146f904fde91b2a2928c1ba0e9b
Oleg Shatniuk <o.shatnuk@gmail.com>
2023-05-05 14:02:46 +0300

    increase listener period

--- indexer/internal/service/service.go
+++ indexer/internal/service/service.go
@@ -988,7 +988,7 @@ func (s *service) ListenBlockchain() error {
 	lastNotificationTime := time.Now().Add(-1 * time.Hour)
 	for {
 		select {
-		case <-time.After(100 * time.Millisecond):
+		case <-time.After(250 * time.Millisecond):
 			current, err := s.checkBlock(lastBlock)
 			if err != nil {
 				err = fmt.Errorf("process block failed: %w", err)
