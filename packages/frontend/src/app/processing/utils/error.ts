d724290fffe5fa4aaacaba7f2a4f68d26b555eb3
alik <colinmilton1@gmail.com>
2023-05-16 12:58:57 +0300

    catchContractError utils update

--- packages/frontend/src/app/processing/utils/error.ts
+++ packages/frontend/src/app/processing/utils/error.ts
@@ -1,8 +1,10 @@
 
-import { serializeError } from '@metamask/rpc-errors'
-import { ContractTransaction } from 'ethers'
+import { JsonRpcError, serializeError } from '@metamask/rpc-errors'
+import { Contract, ContractTransaction } from 'ethers'
 
 import { wagmiClient } from '../../config/web3Modal'
+
+const TWO_MINUTES = 120_000 as const
 
 export const wait = (miliseconds: number) => new Promise<void>((resolve) => {
   setTimeout(() => resolve(), miliseconds)
@@ -10,44 +12,82 @@ export const wait = (miliseconds: number) => new Promise<void>((resolve) => {
 
 const fallbackError = { code: 500, message: 'unknown' }
 
-export const catchContractGetterError = async <T>(call: () => Promise<T>) => {
+const stringifyContractError = (error: any) => {
+  let message = 'Unknown'
+  if ('code' in error && error.code === 'ACTION_REJECTED') {
+    message = 'Contract call rejected by user.'
+  }
+
+  const serializedError = serializeError(error, { fallbackError })
+
+  if (serializedError.code === 500 && (serializedError.data as any)?.cause?.error?.data?.message) {
+    const rawMessage: string = (serializedError.data as any).cause.error.data.message
+    // vm error is truncated and useless
+    message = rawMessage.split(', vm error:')[0]
+  } else if (serializedError.code === 504) {
+    message = serializedError.message
+  } else {
+    message = `Contract call failed. Please try again. Reason: ${serializedError.message}`
+  }
+
+  return message
+}
+
+export const catchContractGetterError = async <R = any>({
+  contract,
+  method
+}: {
+  contract: Contract
+  method: keyof Contract
+},
+  ...args: any[]
+): Promise<R> => {
   try {
-    return await call()
-  } catch (error) {
+    await contract.callStatic[method](...args)
+    return contract[method](...args)
+  } catch (error: any) {
     console.error(error)
 
-    const { message } = serializeError(error, { fallbackError })
-    throw new Error(`Contract request failed. Please try again. Reason: ${message}`)
+    throw new Error(stringifyContractError(error))
   }
 }
 
-export const catchContractCallError = async (
-  call: () => Promise<ContractTransaction>,
-  options?: { ignoreTxFailture: boolean }
+export const catchContractCallError = async ({
+  contract,
+  method,
+  ignoreTxFailture
+}: {
+  contract: Contract
+  method: keyof Contract
+  ignoreTxFailture?: boolean
+},
+...args: any[]
 ) => {
   try {
-    const tx = await call()
+    await contract.callStatic[method](...args)
+    const tx: ContractTransaction = await contract[method](...args)
 
-    if (options?.ignoreTxFailture) {
+    if (ignoreTxFailture) {
       return await tx.wait()
     }
+
     return await catchTxFailture(tx.hash)
   } catch (error: any) {
     console.error(error)
 
-    if ('code' in error && error.code === 'ACTION_REJECTED') {
-      throw new Error('Contract call rejected by user.')
-    }
-
-    const { message } = serializeError(error, { fallbackError })
-    throw new Error(`Contract call failed. Please try again. Reason: ${message}`)
+    throw new Error(stringifyContractError(error))
   }
 }
 
 export const catchTxFailture = async (txHash: string) => {
   let receipt = null
+  const start = Date.now()
 
   while (receipt === null) {
+    if (Date.now() - start > TWO_MINUTES) {
+      throw new JsonRpcError(504, 'The transaction is taking too long to execute')
+    }
+
     receipt = await wagmiClient.provider.getTransactionReceipt(txHash)
 
     if (receipt === null) {
