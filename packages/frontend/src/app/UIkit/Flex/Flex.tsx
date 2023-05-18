67f02348e1c0d87d65a9a75252f970cc5256fc29
alik <colinmilton1@gmail.com>
2023-05-19 00:20:58 +0300

    Flex added to uikit

---
+++ packages/frontend/src/app/UIkit/Flex/Flex.tsx
@@ -0,0 +1,30 @@
+
+import { CSS } from '@stitches/react'
+import React, { PropsWithChildren } from 'react'
+
+import { StyledDiv } from './Flex.styles'
+
+interface FlexProps extends PropsWithChildren {
+  css?: CSS
+  gap?: CSS['gap']
+  alignItems?: CSS['alignItems']
+  justifyContent?: CSS['justifyContent']
+  flexDirection?: CSS['flexDirection']
+  flexWrap?: CSS['flexWrap']
+  w100?: boolean
+  h100?: boolean
+}
+
+export const Flex: React.FC<FlexProps> = ({ children, h100, w100, css, ...props }) => {
+  return (
+    <StyledDiv css={{
+      ...props,
+      ...css,
+      height: h100 ? '100%' : undefined,
+      width: w100 ? '100%' : undefined
+    }}
+    >
+      {children}
+    </StyledDiv>
+  )
+}
