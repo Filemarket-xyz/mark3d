c264af3129028299442d5fb3e4a2632e0199089d
alik <colinmilton1@gmail.com>
2023-05-19 18:10:53 +0300

    InfiniteScroll fixes

--- packages/frontend/src/app/UIkit/InfiniteScroll/InfiniteScroll.tsx
+++ packages/frontend/src/app/UIkit/InfiniteScroll/InfiniteScroll.tsx
@@ -1,10 +1,11 @@
 
 import { CSS } from '@stitches/react'
-import React, { ReactNode, useCallback, useEffect, useMemo, useRef } from 'react'
+import React, { ReactNode, useCallback, useMemo, useRef } from 'react'
 import useVirtual, { type Item } from 'react-cool-virtual'
 
+import { useAfterDidMountEffect } from '../../hooks/useDidMountEffect'
 import { Loading } from '../Loading'
-import { StyledInnerDiv } from './InfiniteScroll.styles'
+import { StyledInnerDiv, StyledTrigger } from './InfiniteScroll.styles'
 
 interface InfiniteScrollProps {
   currentItemCount: number
@@ -23,7 +24,7 @@ export const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
   fetchMore,
   listCss
 }) => {
-  const loadingTriggerRef = useRef<HTMLDivElement>(null)
+  const loadingTriggerRef = useRef<HTMLSpanElement>(null)
   const { outerRef, innerRef, items } = useVirtual<HTMLDivElement, HTMLDivElement>({
     itemCount: currentItemCount
   })
@@ -36,10 +37,10 @@ export const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
 
   const observer = useMemo(() => new IntersectionObserver(observerCallback, { threshold: 0 }), [observerCallback])
 
-  useEffect(() => {
-    if (loadingTriggerRef.current) {
-      observer.observe(loadingTriggerRef.current)
-    }
+  useAfterDidMountEffect(() => {
+    if (!loadingTriggerRef.current) return
+
+    observer.observe(loadingTriggerRef.current)
 
     return () => observer.disconnect()
   }, [loadingTriggerRef.current, observer])
@@ -50,16 +51,18 @@ export const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
         ref={innerRef}
         css={{
           ...listCss,
+          position: 'relative',
           height: 'auto !important'
         }}
       >
-        {items.map(render)}
+        {/* items.length may be incorrect at first render if there was a larger value at the previous render */}
+        {items.length <= currentItemCount && items.map(render)}
+        {!!items.length && hasMore && (
+          <Loading isLoading={isLoading}>
+            <StyledTrigger ref={loadingTriggerRef} />
+          </Loading>
+        )}
       </StyledInnerDiv>
-      {!!currentItemCount && hasMore && (
-        <Loading isLoading={isLoading}>
-          <div ref={loadingTriggerRef} />
-        </Loading>
-      )}
     </div>
   )
 }
