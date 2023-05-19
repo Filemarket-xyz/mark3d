a2207275c6fcdb6ce4ce8401eadb1d37da1e102e
alik <colinmilton1@gmail.com>
2023-05-19 19:11:57 +0300

    CardsPlaceholder replaced by CardsSkeletonLoading

--- packages/frontend/src/app/pages/ProfilePage/sections/OwnedSection.tsx
+++ packages/frontend/src/app/pages/ProfilePage/sections/OwnedSection.tsx
@@ -7,7 +7,7 @@ import { styled } from '../../../../styles'
 import { NFTCard } from '../../../components'
 import Plug from '../../../components/Plug/Plug'
 import { useCollectionAndTokenListStore } from '../../../hooks'
-import { Button, CardsPlaceholder, NavButton, Txt } from '../../../UIkit'
+import { Button, CardsSkeletonLoading, NavButton, Txt } from '../../../UIkit'
 import { Params } from '../../../utils/router/Params'
 import { CardsContainer } from '../../MarketPage/NftSection'
 
@@ -22,67 +22,47 @@ const NoNftContainer = styled('div', {
 })
 
 export const OwnedSection = observer(() => {
-  const { isLoading, nftCards } = useCollectionAndTokenListStore()
+  const collectionAndTokenListStore = useCollectionAndTokenListStore()
   const { profileAddress } = useParams<Params>()
   const { address: currentAddress } = useAccount()
   const navigate = useNavigate()
 
-  const generateContentIfNoCards = () => {
-    if (profileAddress === currentAddress) {
-      return (
-        <>
-          <Plug
-            header={'You don`t have any NFTs '}
-            mainText={'Create your own NFT or go to the market to find something amazing'}
-            buttonsBlock={(
-              <>
-                <Button primary onClick={() => navigate('/market')}>
-                  <Txt primary1>3D Market</Txt>
-                </Button>
-                <Button onClick={() => navigate('/create')}>
-                  <Txt primary1>Create</Txt>
-                </Button>
-              </>
-            )}
-          />
-          <NavButton
-            primary
-            to={'/create/nft'}
-            css={{ textDecoration: 'none', marginBottom: '$3' }}
-          >
-            Create NFT
-          </NavButton>
-        </>
-      )
-    }
-
-    return (
-      <Plug
-        header={'You don`t have any NFTs '}
-        mainText={'Create your own NFT or go to the market to find something amazing'}
-        buttonsBlock={(
-          <>
-            <Button primary onClick={() => navigate('/market')}>
-              <Txt primary1>3D Market</Txt>
-            </Button>
-            <Button onClick={() => navigate('/create')}>
-              <Txt primary1>Create</Txt>
-            </Button>
-          </>
-        )}
-      />
-    )
-  }
-
   return (
     <CardsContainer>
-      {isLoading ? (
-        <CardsPlaceholder cardsAmount={5} />
-      ) : !!nftCards.length ? (
-        nftCards.map((card, i) => <NFTCard {...card} key={i} />)
-      ) : (
-        <NoNftContainer>{generateContentIfNoCards()}</NoNftContainer>
-      )}
+      <CardsSkeletonLoading
+        isLoading={collectionAndTokenListStore.isLoading}
+        count={5}
+      >
+        {!!collectionAndTokenListStore.nftCards.length ? (
+          collectionAndTokenListStore.nftCards.map((card, i) => <NFTCard {...card} key={i} />)
+        ) : (
+          <NoNftContainer>
+            <Plug
+              header={'You don`t have any NFTs '}
+              mainText={'Create your own NFT or go to the market to find something amazing'}
+              buttonsBlock={(
+                <>
+                  <Button primary onClick={() => navigate('/market')}>
+                    <Txt primary1>3D Market</Txt>
+                  </Button>
+                  <Button onClick={() => navigate('/create')}>
+                    <Txt primary1>Create</Txt>
+                  </Button>
+                </>
+              )}
+            />
+            {profileAddress === currentAddress && (
+              <NavButton
+                primary
+                to={'/create/nft'}
+                css={{ textDecoration: 'none', marginBottom: '$3' }}
+              >
+                Create NFT
+              </NavButton>
+            )}
+          </NoNftContainer>
+        )}
+      </CardsSkeletonLoading>
     </CardsContainer>
   )
 })
--- packages/frontend/src/app/pages/ProfilePage/sections/TransfersSection.tsx
+++ packages/frontend/src/app/pages/ProfilePage/sections/TransfersSection.tsx
@@ -6,14 +6,14 @@ import { useAccount } from 'wagmi'
 import { TransferCard } from '../../../components/MarketCard/TransferCard'
 import Plug from '../../../components/Plug/Plug'
 import { useUserTransferStore } from '../../../hooks/useUserTransfers'
-import { Button, CardsPlaceholder, Txt } from '../../../UIkit'
+import { Button, CardsSkeletonLoading, Txt } from '../../../UIkit'
 import { Params } from '../../../utils/router/Params'
 import { CardsContainer } from '../../MarketPage/NftSection'
 
 const TransfersSection = observer(() => {
   const { address: currentAddress } = useAccount()
   const { profileAddress } = useParams<Params>()
-
+  const { transferCards, isLoading } = useUserTransferStore(profileAddress)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -22,31 +22,29 @@ const TransfersSection = observer(() => {
     }
   }, [])
 
-  const { transferCards, isLoading } = useUserTransferStore(profileAddress)
-
   return (
-    <>
-      {isLoading ? (
-        <CardsPlaceholder cardsAmount={5} />
-      ) : transferCards.length ? (
+    <CardsSkeletonLoading isLoading={isLoading} count={5}>
+      {transferCards.length ? (
         <CardsContainer>
-          {transferCards.map((card, i) => (
-            <TransferCard {...card} key={i} />
-          ))}
+          {transferCards.map((card, i) => <TransferCard {...card} key={i} />)}
         </CardsContainer>
       ) : (
-        <Plug header={'You don`t have any activity'}
-              mainText={'Get started by creating your own NFT or go to the market to find something amazing'}
-              buttonsBlock={<>
-                <Button primary onClick={() => { navigate('/market') }}>
-                  <Txt primary1>3D Market</Txt>
-                </Button>
-                <Button onClick={() => { navigate('/create') }}>
-                  <Txt primary1>Create</Txt>
-                </Button></>}
+        <Plug
+          header={'You don`t have any activity'}
+          mainText={'Get started by creating your own NFT or go to the market to find something amazing'}
+          buttonsBlock={(
+            <>
+              <Button primary onClick={() => { navigate('/market') }}>
+                <Txt primary1>3D Market</Txt>
+              </Button>
+              <Button onClick={() => { navigate('/create') }}>
+                <Txt primary1>Create</Txt>
+              </Button>
+            </>
+          )}
         />
       )}
-    </>
+    </CardsSkeletonLoading>
   )
 })
 
