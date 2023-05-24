1fbaba59f2dce7dc58666d19b7d3b17f17f544bf
LewinUp <raft.took@yandex.ru>
2023-05-25 00:36:24 +0300

    feat: refactor

--- packages/frontend/src/app/UIkit/Form/Input.tsx
+++ packages/frontend/src/app/UIkit/Form/Input.tsx
@@ -1,6 +1,17 @@
 import { styled } from '../../../styles'
 import { textVariant } from '../Txt'
 
+// const glow = keyframes({
+//   '0%': {
+//     borderColor: '#393',
+//     boxShadow: '0 0 5px rgba(0,255,0,.2), inset 0 0 5px rgba(0,255,0,.1), 0 2px 0 #000'
+//   },
+//   '100%': {
+//     borderColor: '#6f6',
+//     boxShadow: '0 0 20px rgba(0,255,0,.6), inset 0 0 10px rgba(0,255,0,.4), 0 2px 0 #000'
+//   }
+// })
+
 export const inputStyles = {
   backgroundColor: '$white',
   borderRadius: '$3',
@@ -11,6 +22,7 @@ export const inputStyles = {
   color: '$blue900',
   border: '2px solid transparent',
   boxShadow: '0px 0px 15px rgba(19, 19, 45, 0.05)',
+  transition: 'all 0.5s',
   '&:placeholder': {
     color: '$gray400'
   },
@@ -41,6 +53,7 @@ export const inputStyles = {
       }
     }
   }
+
 }
 
 export const Input = styled('input', {
