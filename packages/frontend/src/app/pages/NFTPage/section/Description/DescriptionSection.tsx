9ac7af563e1d62f303ce6ddba85878cce064d568
LewinUp <raft.took@yandex.ru>
2023-05-24 14:12:42 +0300

    feat: edit colors and text

--- packages/frontend/src/app/pages/NFTPage/section/Description/DescriptionSection.tsx
+++ packages/frontend/src/app/pages/NFTPage/section/Description/DescriptionSection.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import { styled } from '../../../../../styles'
 import { useTokenStore } from '../../../../hooks/useTokenStore'
 import { Params } from '../../../../utils/router/Params'
 import { GridBlock, P, PropertyTitle } from '../../helper/styles/style'
+
+const DescriptionSectionStyle = styled(GridBlock, {
+  paddingTop: '32px',
+  paddingBottom: '32px',
+  '@md': {
+    paddingTop: '16px',
+    paddingBottom: '16px'
+  },
+  '@sm': {
+    paddingTop: '8px',
+    paddingBottom: '8px'
+  }
+})
 
 const DescriptionSection = () => {
   const { collectionAddress, tokenId } = useParams<Params>()
@@ -12,10 +26,10 @@ const DescriptionSection = () => {
   return (
     <>
       {token?.description && (
-        <GridBlock style={{ gridArea: 'Description' }}>
+        <DescriptionSectionStyle style={{ gridArea: 'Description' }}>
           <PropertyTitle>Description</PropertyTitle>
           <P>{token?.description}</P>
-        </GridBlock>
+        </DescriptionSectionStyle>
       )}
     </>
   )
