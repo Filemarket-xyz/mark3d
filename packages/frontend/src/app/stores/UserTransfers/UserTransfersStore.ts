4a4c3bc5b0b80dd1ce25fbc0c11978510d6b2fa4
alik <colinmilton1@gmail.com>
2023-05-26 21:56:42 +0300

    transferCards props update

--- packages/frontend/src/app/stores/UserTransfers/UserTransfersStore.ts
+++ packages/frontend/src/app/stores/UserTransfers/UserTransfersStore.ts
@@ -11,7 +11,7 @@ import {
   IStoreRequester,
   RequestContext,
   storeRequest,
-  storeReset
+  storeReset,
 } from '../../utils/store'
 import { lastItem } from '../../utils/structs'
 import { formatCurrency } from '../../utils/web3/currency'
@@ -26,16 +26,16 @@ const convertTransferToTransferCards = (target: 'incoming' | 'outgoing') => {
     status: transfer.order?.id === 0 ? eventOptions[0] : eventOptions[1],
     button: {
       link: `/collection/${transfer.collection?.address}/${transfer.token?.tokenId}`,
-      text: 'Go to page'
+      text: 'Go to page',
     },
-    collection: `${transfer.collection?.name}`,
+    collectionName: transfer.collection?.name ?? '',
     imageURL: getHttpLinkFromIpfsString(transfer.token?.image ?? ''),
-    title: `${transfer.token?.name}`,
+    title: transfer.token?.name ?? '',
     user: {
-      username: reduceAddress(transfer.token?.owner ?? '—'),
-      img: getProfileImageUrl(transfer.token?.owner ?? '')
+      address: reduceAddress(transfer.token?.owner ?? '—'),
+      img: getProfileImageUrl(transfer.token?.owner ?? ''),
     },
-    price: transfer.order?.price ? formatCurrency(transfer.order?.price) : undefined
+    price: transfer.order?.price ? formatCurrency(transfer.order?.price) : undefined,
   })
 }
 
@@ -54,7 +54,7 @@ export class UserTransferStore implements IActivateDeactivate<[string]>, IStoreR
   constructor({ errorStore }: { errorStore: ErrorStore }) {
     this.errorStore = errorStore
     makeAutoObservable(this, {
-      errorStore: false
+      errorStore: false,
     })
   }
 
@@ -71,7 +71,7 @@ export class UserTransferStore implements IActivateDeactivate<[string]>, IStoreR
     storeRequest<TransfersResponseV2>(
       this,
       api.v2.transfersDetail(this.address, { outgoingLimit: 10, incomingLimit: 10 }),
-      (data) => this.setData(data)
+      (data) => this.setData(data),
     )
   }
 
@@ -90,9 +90,9 @@ export class UserTransferStore implements IActivateDeactivate<[string]>, IStoreR
         lastIncomingTransferId,
         lastOutgoingTransferId,
         outgoingLimit: 10,
-        incomingLimit: 10
+        incomingLimit: 10,
       }),
-      (data) => this.addData(data)
+      (data) => this.addData(data),
     )
   }
 
@@ -119,10 +119,10 @@ export class UserTransferStore implements IActivateDeactivate<[string]>, IStoreR
     const { incoming = [], outgoing = [] } = this.data
 
     const incomingCards = incoming.map<TransferCardProps>(
-      convertTransferToTransferCards('incoming')
+      convertTransferToTransferCards('incoming'),
     )
     const outgoingCards = outgoing.map<TransferCardProps>(
-      convertTransferToTransferCards('outgoing')
+      convertTransferToTransferCards('outgoing'),
     )
 
     return incomingCards.concat(outgoingCards)
