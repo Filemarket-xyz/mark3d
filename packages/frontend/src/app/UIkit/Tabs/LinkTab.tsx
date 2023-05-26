d9439ed8ca8d2e66ab9396b0297e50d7804b7066
alik <colinmilton1@gmail.com>
2023-05-26 21:47:25 +0300

    Tabs updates & fixes

--- packages/frontend/src/app/UIkit/Tabs/LinkTab.tsx
+++
@@ -1,7 +0,0 @@
-import { Tab } from '@mui/material'
-import React, { ComponentProps } from 'react'
-
-/** Should pass any content as icon property */
-export default function LinkTab(props: ComponentProps<typeof Tab>) {
-  return <Tab sx={{ textTransform: 'none' }} LinkComponent={'a'} {...props} />
-}
