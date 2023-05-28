8497b83d954ee349c76a94549cefd2b8d95decfb
alik <colinmilton1@gmail.com>
2023-05-28 21:12:31 +0300

    worker self close removed

--- packages/crypto/src/dedicated-wokrers/rsa.worker.js
+++ packages/crypto/src/dedicated-wokrers/rsa.worker.js
@@ -5,7 +5,7 @@ import { rsaGenerateKeyPair } from '../lib/rsa'
 self.onmessage = ({ data }) => {
   rsaGenerateKeyPair(data.seed)
     .then((keyPair) => self.postMessage(keyPair))
-    .then(() => self.close())
+    // .then(() => self.close())
 }
 
 self.addEventListener("unhandledrejection", event => {
