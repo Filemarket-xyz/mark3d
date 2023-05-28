deb948df4e7996399f1768458c62df740f877ad9
alik <colinmilton1@gmail.com>
2023-05-28 23:15:26 +0300

    logs added

--- packages/crypto/src/dedicated-wokrers/rsa.worker.js
+++ packages/crypto/src/dedicated-wokrers/rsa.worker.js
@@ -2,13 +2,13 @@ import { rsaGenerateKeyPair } from '../lib/rsa'
 // we should leave this in the js file to make the worker work both locally and in the prod version
 // because of the file extension when creating the worker - new Worker('./path/to/worker.js')
 
+
 self.onmessage = ({ data }) => {
   rsaGenerateKeyPair(data.seed)
     .then((keyPair) => self.postMessage(keyPair))
-    // .then(() => self.close())
+    .then(() => self.close())
 }
 
-self.addEventListener("unhandledrejection", event => {
-  postMessage({type: "error", error: event.reason});
-  event.preventDefault();
+self.addEventListener("unhandledRejection", event => {
+  throw new Error(event.reason)
 });
--- packages/crypto/src/lib/eft-derivation.ts
+++ packages/crypto/src/lib/eft-derivation.ts
@@ -64,6 +64,7 @@ const eftRsaDerivationAux = async (
 
   return new Promise((resolve, reject) => {
     const rsaWorker = new Worker(rsaWorkerUrl, { type: 'module' })
+    console.log({rsaWorker, rsaWorkerUrl})
     rsaWorker.addEventListener('error', console.error)
     rsaWorker.addEventListener('message', console.log)
     rsaWorker.onmessage = (e) => resolve(e.data)
