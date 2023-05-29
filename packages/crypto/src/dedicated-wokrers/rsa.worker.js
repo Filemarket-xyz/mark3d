7f0eebe6913a4e91a8c605b669d81769316446c6
alik <colinmilton1@gmail.com>
2023-05-29 20:16:05 +0300

    dynamic import update in worker

--- packages/crypto/src/dedicated-wokrers/rsa.worker.js
+++ packages/crypto/src/dedicated-wokrers/rsa.worker.js
@@ -1,37 +1,6 @@
 self.onmessage = async ({ data }) => {
-  const {pki, random, asn1} = await import('node-forge')
-  const {Buffer} = await import('buffer')
-
-  const rsaGenerateKeyPair = async (seed) => {
-    const seedHex = Buffer.from(seed).toString('hex')
-    // seed pseudo random number generation
-    const prng = random.createInstance();
-    prng.seedFileSync = () => seedHex
-    // workers 0 is necessary for deterministic key generation
-    const key = pki.rsa.generateKeyPair({bits: 4096, workers: 0, prng})
-
-    return {
-      pub: rsaPublicSubjectKeyInfoToBytes(key.publicKey),
-      priv: rsaPrivateKeyPKCS8ToBytes(key.privateKey),
-    }
-  }
-
-  const rsaPrivateKeyPKCS8ToBytes = (privateKey) => {
-    const privateKeyAsn1 = pki.privateKeyToAsn1(privateKey);
-    // privateKey comes without additional info about it and window.crypto.importKey can't recognize it
-    // so we need to wrap this privateKey in ASN.1 object with additional data (version, algorithmId)
-    const privateKeyInfoAsn1 = pki.wrapRsaPrivateKey(privateKeyAsn1);
-    const privateKeyDer = asn1.toDer(privateKeyInfoAsn1);
-
-    return Buffer.from(privateKeyDer.toHex(), 'hex')
-  }
-
-  const rsaPublicSubjectKeyInfoToBytes = (publicKey) => {
-    const publicKeyAsn1 = pki.publicKeyToAsn1(publicKey);
-    const publicKeyDer = asn1.toDer(publicKeyAsn1);
-
-    return Buffer.from(publicKeyDer.toHex(), 'hex')
-  }
+  const url =  new URL('../lib/rsa', import.meta.url)
+  const { rsaGenerateKeyPair } = await import(url.href)
 
   const keyPair = await rsaGenerateKeyPair(data.seed)
   self.postMessage(keyPair)
