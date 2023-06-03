95b28c7a9e1cb3d1f06fc0c3a0ac6f3defa21ea3
alik <colinmilton1@gmail.com>
2023-06-03 15:23:52 +0300

    useMintCollection update

--- packages/frontend/src/app/processing/nft-interaction/useMintCollection.ts
+++ packages/frontend/src/app/processing/nft-interaction/useMintCollection.ts
@@ -2,13 +2,14 @@ import assert from 'assert'
 import { ContractReceipt } from 'ethers'
 import { randomBytes } from 'ethers/lib/utils'
 import { useCallback } from 'react'
+import { useAccount } from 'wagmi'
 
 import { mark3dConfig } from '../../config/mark3d'
 import { useStatusState } from '../../hooks'
 import { useAccessTokenContract } from '../contracts'
 import { Mark3dAccessTokenEventNames } from '../types'
 import { callContract } from '../utils'
-import { assertContract, assertSigner } from '../utils/assert'
+import { assertAccount, assertContract, assertSigner } from '../utils/assert'
 import { normalizeCounterId } from '../utils/id'
 import { useUploadLighthouse } from './useUploadLighthouse'
 
@@ -27,6 +28,7 @@ interface CreateCollectionResult {
 }
 
 export function useMintCollection(form: CreateCollectionForm = {}) {
+  const { address } = useAccount()
   const { contract, signer } = useAccessTokenContract()
   const { wrapPromise, ...statuses } = useStatusState<CreateCollectionResult>()
   const upload = useUploadLighthouse()
@@ -34,6 +36,7 @@ export function useMintCollection(form: CreateCollectionForm = {}) {
   const mintCollection = useCallback(wrapPromise(async () => {
     assertContract(contract, mark3dConfig.accessToken.name)
     assertSigner(signer)
+    assertAccount(address)
     assert(name && symbol && image, 'CreateCollection form is not filled')
 
     const metadata = await upload({
@@ -52,8 +55,8 @@ export function useMintCollection(form: CreateCollectionForm = {}) {
       symbol,
       metadata.url,
       metadata.url,
+      address,
       '0x00',
-      { gasPrice: mark3dConfig.gasPrice },
     )
 
     const createCollectionEvent = receipt.events
