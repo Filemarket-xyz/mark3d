789653855a4a2c8d67f37333fe58079a2a5f569d
LewinUp <raft.took@yandex.ru>
2023-06-11 14:25:15 +0300

    feat: remove getAccessPage

--- packages/frontend/src/app/pages/router.tsx
+++ packages/frontend/src/app/pages/router.tsx
@@ -6,64 +6,55 @@ import CollectionPage from './CollectionPage/CollectionPage'
 import { collectionPageRoutes } from './CollectionPage/routes'
 import { createRoutes } from './CreatePage/routes'
 import ExplorerPage from './ExplorerPage/ExplorerPage'
-import GetAccessPage from './GetAccessPage/GetAccessPage'
 import MainPage from './MainPage/MainPage'
 import MarketPage from './MarketPage/MarketPage'
 import { marketRoutes } from './MarketPage/routes'
 import NFTPage from './NFTPage/NFTPage'
 import ProfilePage from './ProfilePage/ProfilePage'
 import { profileRoutes } from './ProfilePage/routes'
-import SuccessSubscribePage from './SuccessSubscribePage/SuccessSubscribePage'
 
 const routes: RouteObject[] = [
   {
-    path: '/',
-    element: <GetAccessPage />,
+    path: 'explorer',
+    element: <ExplorerPage />,
   },
   {
-    path: 'successGetAccess',
-    element: <SuccessSubscribePage />,
+    path: '/',
+    element: <MainPage />,
   },
+  {
+    path: 'market',
+    element: <MarketPage />,
+    children: marketRoutes,
+  },
+  {
+    path: 'create',
+    children: createRoutes,
+  },
+  {
+    path: `profile/:${Params.profileAddress}`,
+    element: <ProfilePage />,
+    children: profileRoutes,
+  },
+  {
+    path: `collection/:${Params.collectionAddress}`,
+    element: <CollectionPage />,
+    children: collectionPageRoutes,
+  },
+  {
+    path: `collection/:${Params.collectionAddress}/:${Params.tokenId}`,
+    element: <NFTPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to={''} />,
+  },
+]
+
+export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [
-      {
-        path: 'explorer',
-        element: <ExplorerPage />,
-      },
-      {
-        path: 'mainpage',
-        element: <MainPage />,
-      },
-      {
-        path: 'market',
-        element: <MarketPage />,
-        children: marketRoutes,
-      },
-      {
-        path: 'create',
-        children: createRoutes,
-      },
-      {
-        path: `profile/:${Params.profileAddress}`,
-        element: <ProfilePage />,
-        children: profileRoutes,
-      },
-      {
-        path: `collection/:${Params.collectionAddress}`,
-        element: <CollectionPage />,
-        children: collectionPageRoutes,
-      },
-      {
-        path: `collection/:${Params.collectionAddress}/:${Params.tokenId}`,
-        element: <NFTPage />,
-      },
-      {
-        path: '*',
-        element: <Navigate to={''} />,
-      },
-    ],
+    children: routes,
   },
-]
-export const router = createBrowserRouter(routes)
+])
