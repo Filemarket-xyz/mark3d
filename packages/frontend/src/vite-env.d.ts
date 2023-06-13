96ae71196fa7c504042017fb6a858a43b600c817
alik <colinmilton1@gmail.com>
2023-06-13 22:27:46 +0300

    registerProperty declaration added

--- packages/frontend/src/vite-env.d.ts
+++ packages/frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare namespace CSS {
+  function registerProperty (propertyDefinition: {
+    name: string
+    syntax?: string
+    inherits: boolean
+    initialValue?: string
+  }): void
+}
