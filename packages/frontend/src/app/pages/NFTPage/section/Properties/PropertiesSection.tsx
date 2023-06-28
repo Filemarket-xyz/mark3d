e631b223d28c2f2538222566f5d57828c2c670a1
LewinUp <raft.took@yandex.ru>
2023-06-28 12:03:57 +0300

    feat: remove properties if doesn't exist

--- packages/frontend/src/app/pages/NFTPage/section/Properties/PropertiesSection.tsx
+++ packages/frontend/src/app/pages/NFTPage/section/Properties/PropertiesSection.tsx
@@ -35,34 +35,38 @@ interface PropertiesProps {
 
 const PropertiesSection: FC<PropertiesProps> = ({ properties, rankings }) => {
   return (
-    <GridBlock style={{ gridArea: 'Properties' }}>
-      <PropertyTitle>Properties</PropertyTitle>
-      <PropertiesStyle>
-        <div className="overflow">
-          {properties?.map((property) => {
-            return (
-              <PropertiesCard
-                key={property.traitType}
-                type={property.traitType ?? ''}
-                rare={property.value ?? ''}
-                maxValue={property.traitTotal.toString()}
-                minValue={property.traitValueCount?.toString()}
-              />
-            )
-          })}
-          {rankings?.map((property) => {
-            return (
-              <PropertiesCard
-                key={property.traitType}
-                rare={property.traitType ?? ''}
-                maxValue={property.max_value?.includes(',') ? parseFloat(property.max_value ?? '0').toFixed(2) : parseInt(property.max_value ?? '0').toString()}
-                minValue={property.value?.includes(',') ? parseFloat(property.value ?? '0').toFixed(2) : parseInt(property.value ?? '0').toString()}
-              />
-            )
-          })}
-        </div>
-      </PropertiesStyle>
-    </GridBlock>
+    <>
+      {((properties && properties.length > 0) || (rankings && rankings.length > 0)) && (
+        <GridBlock style={{ gridArea: 'Properties' }}>
+          <PropertyTitle>Properties</PropertyTitle>
+          <PropertiesStyle>
+            <div className="overflow">
+              {properties?.map((property) => {
+                return (
+                  <PropertiesCard
+                    key={property.traitType}
+                    type={property.traitType ?? ''}
+                    rare={property.value ?? ''}
+                    maxValue={property.traitTotal.toString()}
+                    minValue={property.traitValueCount?.toString()}
+                  />
+                )
+              })}
+              {rankings?.map((property) => {
+                return (
+                  <PropertiesCard
+                    key={property.traitType}
+                    rare={property.traitType ?? ''}
+                    maxValue={property.max_value?.includes(',') ? parseFloat(property.max_value ?? '0').toFixed(2) : parseInt(property.max_value ?? '0').toString()}
+                    minValue={property.value?.includes(',') ? parseFloat(property.value ?? '0').toFixed(2) : parseInt(property.value ?? '0').toString()}
+                  />
+                )
+              })}
+            </div>
+          </PropertiesStyle>
+        </GridBlock>
+      )}
+    </>
   )
 }
 
